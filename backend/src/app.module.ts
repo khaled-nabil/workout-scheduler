8473f87c2d1a27ec9535ae3be23777ab741c3822
KhaledNabil <khaled.nabil.k@gmail.com>
2020-09-20 21:06:43 +0200

    initialize typeORM with mongoDB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { WorkoutModule } from './workout/workout.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [WorkoutModule],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mongodb',
+            host: 'localhost',
+            port: 27017,
+            username: 'root',
+            password: 'pass',
+            database: 'workout-scheduler',
+            entities: ['src/**/**.entity{.ts,.js}'],
+            synchronize: true,
+        }),
+        WorkoutModule,
+    ],
 })
 export class AppModule {}
