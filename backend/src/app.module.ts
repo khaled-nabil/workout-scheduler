12f979fa3e73d0dee65766e2944cadcda11776a1
KhaledNabil <khaled.nabil.k@gmail.com>
2020-09-21 00:29:08 +0200

    add not found interceptor

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { WorkoutModule } from './workout/workout.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { NotFoundInterceptor } from './app.interceptor';
 
 @Module({
     imports: [TypeOrmModule.forRoot(), WorkoutModule],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: NotFoundInterceptor,
+        },
+    ],
 })
 export class AppModule {}
