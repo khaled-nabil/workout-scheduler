aabf7363a168e38524d7642c6c983efac987f71b
KhaledNK <khaled.nabil.k@gmail.com>
2020-09-22 15:00:24 +0200

    styling workout cards

--- frontend/src/views/organisms/WorkoutCards/index.tsx
+++ frontend/src/views/organisms/WorkoutCards/index.tsx
@@ -1,20 +1,24 @@
 import moment from 'moment';
 import React from 'react';
 import { Workout } from 'types/workout';
-import { Paragraph, Image, Link } from 'views/atoms';
+import { Paragraph, Image, Link, Box } from 'views/atoms';
 
 const WorkoutCards = ({ workouts }: { workouts: Array<Workout> }) => (
   <>
     {workouts?.map((workout, i) => (
       <Link
         href={`/workout/${workout._id}`}
-        width={[1 / 2, 1 / 3, 1 / 5]}
+        width={[1 / 2, 1 / 3, 1 / 4, 1 / 5]}
         key={i}
       >
-        <Image src={workout.picture} />
-        <Paragraph>{workout.name}</Paragraph>
-        <Paragraph>{workout.category}</Paragraph>
-        <Paragraph>{moment(workout.startDate).format('YYYY-MM-DD')}</Paragraph>
+        <Box m={[2, null, null, 3]}>
+          <Image width={'100%'} src={workout.picture} />
+          <Paragraph>{workout.name}</Paragraph>
+          <Paragraph>{workout.category}</Paragraph>
+          <Paragraph>
+            {moment(workout.startDate).format('YYYY-MM-DD')}
+          </Paragraph>
+        </Box>
       </Link>
     ))}
   </>
